e840ed61416c97d4babe18a854e110ea05a7f7f5
PostResPerditas <hannki_lj@163.com>
2025-07-08 15:51:13 +0800

    update

---
+++ usr/chapter_3/3_3_linear_torch.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+from torch.utils import data
+from d2l import torch as d2l
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+feature, labels = d2l.synthetic_data(true_w, true_b, 1000)
+
